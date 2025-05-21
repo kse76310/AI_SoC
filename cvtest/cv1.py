f760d9f57912cedee2952690d08036cc1316007e
kse76310 <kse76310@naver.com>
2025-05-21 14:24:00 +0900

    test

--- cvtest/cv1.py
+++ cvtest/cv1.py
@@ -1,8 +1,11 @@
 import cv2
 
-img = cv2.imread('c1.jpg')
-gimg = cv2.cvtColor(img,cv2.COLOR_BGR2BGR565)
+s_img = cv2.imread('c1.jpg')
+##c_img = cv2.GaussianBlur(s_img,(7,7),0)
+#c_img = cv2.threshold(s_img,(7,7),0)
+#gimg = cv2.cvtColor(img,cv2.COLOR_BGR2BGR565)
+c_img = cv2.Canny(s_img,100,200)
 
-cv2.imshow("",img)
-cv2.imshow("Converted",gimg)
+cv2.imshow("",s_img)
+cv2.imshow("Converted",c_img)
 cv2.waitKey(0)
--- cvtest/cvtest.py
+++ cvtest/cvtest.py
@@ -10,7 +10,15 @@ cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while cv2.waitKey(1)!=27:
 	ret,f = cam.read()
+	c1 = cv2.cvtColor(f,cv2.COLOR_BGR2GRAY)
+	c2 = c2.GaussianBlur(f,(7,7),0)
+	r,c3 = cv2.threshold(f,120,255,cv2.THRESH_BINARY)
+
 	cv2.imshow('aa',f)
+	cv2.imshow("1",c1)
+	cv2.imshow("2",c2)
+	cv2.imshow("3",c3)
+	
 cv2.imwrite('c1.jpg',f)
 cv2.waitKey()
 cv2.destroyAllWindows()
