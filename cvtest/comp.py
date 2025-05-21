031b677bbd23dd95b2b39efa250c2b06a1eada57
kse76310 <kse76310@naver.com>
2025-05-21 15:40:53 +0900

    test

--- cvtest/comp.py
+++ cvtest/comp.py
@@ -3,9 +3,12 @@ import cv2
 s1 = cv2.imread('c4.jpg')
 s2 = cv2.imread('c5.jpg')
 diff = cv2.absdiff(s1,s2)
-
-cv2.imshow('s1',s1)
-cv2.imshow('s2',s2)
+r,thre = cv2.threshold(diff, 120, 255, cv2.THRESH_BINARY)
+#cv2.imshow('s1',s1)
+#cv2.imshow('s2',s2)
 
 cv2.imshow("diff",diff)
+cv2.imshow("threshold",thre)
+
+
 cv2.waitKey()
