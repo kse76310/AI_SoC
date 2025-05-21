1b9676001d830bf76b9f296bd29c94ea6447be01
kse76310 <kse76310@naver.com>
2025-05-21 16:28:35 +0900

    윤곽선 검출 및 그리기

--- cvtest/comp.py
+++ cvtest/comp.py
@@ -2,17 +2,24 @@ import cv2
 
 s1 = cv2.imread('c4.jpg')
 s2 = cv2.imread('c5.jpg')
-diff = cv2.absdiff(s1,s2)
-r,thre = cv2.threshold(diff, 120, 255, cv2.THRESH_BINARY)
+
+diff = cv2.absdiff(s1, s2)
+
+# 2. 흑백으로 변환
+gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+r,thre = cv2.threshold(gray,120,255,cv2.THRESH_BINARY)
 cann = cv2.Canny(diff,100,200)
+# 윤곽선 검출
+contours, _ = cv2.findContours(thre, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-#cv2.imshow('s1',s1)
-#cv2.imshow('s2',s2)
+# 윤곽선 그리기
+cv2.drawContours(s1, contours, -1, (0, 255, 0), 2)
 
-cv2.imshow("diff",diff)
-cv2.imshow("threshold",thre)
-cv2.imshow("Canny",cann)
+cv2.imshow("diff", diff)
+cv2.imshow("thre", thre)
+cv2.imshow("canny", cann)
+cv2.imshow("contours", s1)
 
-
-
-cv2.waitKey()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
